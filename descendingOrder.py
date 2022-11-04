cb6faaf8cde70ce85fce5c5c80e238be853ba940
Kashish Sharma <116347828+kvshxsh@users.noreply.github.com>
2022-11-04 21:51:26 +0530

    Update descendingOrder.py

--- descendingOrder.py
+++ descendingOrder.py
@@ -3,12 +3,12 @@ num2 = int(input("enter num.2:"))
 num3 = int(input("enter num.3:"))
 if ((num1>num2) and (num1>num3)):
     if (num2>num3):
-        print("%d, %d, %d"%(num1,num2,num3))
+        print("num.1: %d, num2: %d, num3: %d"%(num1,num2,num3))
 elif ((num2>num1) and (num2>num3)):
     if(num1>num3):
-        print("%d, %d, %d"%(num2,num1,num3))
+        print("num.2: %d, num1: %d, num3: %d"%(num2,num1,num3))
 elif ((num3>num2) and (num3>num1)):
     if(num2>num1):
-        print("%d, %d, %d"%(num3,num2,num1))
+        print("num.3: %d, num.2: %d, num.1: %d"%(num3,num2,num1))
 else:
-    print ("can't compare: two or more entered values are identical")
+    print ("can't compare as two or more entered values are identical")
