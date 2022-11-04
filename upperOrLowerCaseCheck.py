37d347ad8c20084d944c68f3a1da2247e5eff019
Kashish Sharma <116347828+kvshxsh@users.noreply.github.com>
2022-11-04 17:40:07 +0530

    Update upperOrLowerCaseCheck.py

--- upperOrLowerCaseCheck.py
+++ upperOrLowerCaseCheck.py
@@ -3,7 +3,9 @@ if (char>='A' and char<= 'Z'):
      print('UPPERCASE')
 elif (char>= 'a' and char<='z'):
      print('LOWERCASE')
+elif(char>'10'):
+     print("you've entered a number with more than one digit")
 elif((char>='0') and (char<='9')):
      print("this is a number.")
 else:
-     print("this is possibly a special character.")
+     print("this could be a special character.")
