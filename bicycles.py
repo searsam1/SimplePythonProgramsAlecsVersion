f52d3f37b296d780eff5c9d75601a466da559a09
Kashish Sharma <116347828+kvshxsh@users.noreply.github.com>
2022-11-05 21:41:33 +0530

    Add files via upload

---
+++ bicycles.py
@@ -0,0 +1,7 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0])   # to print the name of the first bicycle mentioned on the 0th index, i.e, trek
+print(bicycles[0].title()) # to print a capitalized form of 'trek', the 0th element on the list, bicycles
+print(bicycles) 
+print(bicycles[-1]) # will print 'specialized' (3rd element)
+message= f"My first bicycle was a {bicycles[0].title()}." # the variable 'message' contains the 0th index element from the given list, bicycles 
+print(message)
