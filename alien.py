850e97b51131050cea8b2ba90b7e5b210065d99b
Kashish Sharma <116347828+kvshxsh@users.noreply.github.com>
2022-11-11 17:30:18 +0530

    Add files via upload

---
+++ alien.py
@@ -0,0 +1,5 @@
+alien_0 = {'color' : 'green', 'points' : 5}  # the '_0' in 'alien' holds no significance as to if the dictionary was just 'alien'
+print(alien_0['color'])
+print(alien_0['points'],'\n')
+new_points = alien_0['points']
+print(f'You just earned {new_points} points!')
