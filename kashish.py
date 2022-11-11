997502b3ff33647464df436c293f807d7b20646d
Kashish Sharma <116347828+kvshxsh@users.noreply.github.com>
2022-11-11 21:43:54 +0530

    Add files via upload

---
+++ kashish.py
@@ -0,0 +1,5 @@
+kashish = {'first_name' : 'Kashish', 'last_name' : 'Sharma', 'age' : '22', 'city': 'Shimla'}
+print(kashish['first_name'])
+print(kashish['last_name'])
+print(kashish['age'])
+print(kashish['city'])
