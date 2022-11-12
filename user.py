359f779e3ec8f1080a73591f8c9f0d38a2c2223a
Kashish Sharma <116347828+kvshxsh@users.noreply.github.com>
2022-11-12 23:10:25 +0530

    Add files via upload

---
+++ user.py
@@ -0,0 +1,9 @@
+user_0 = {
+    'username' : 'kvshxsh',
+    'first_name' : 'Kashish',
+    'last_name' : 'Sharma'
+}
+
+for key, value in user_0.items():
+    print(f'\nKey: {key}')
+    print(f'Value: {value}')
